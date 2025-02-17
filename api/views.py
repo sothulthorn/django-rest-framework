ef8f1b0492c2e08043cdd468142a63aaeeeb0e50
sothulthorn <thornsothul@gmail.com>
2025-02-17 22:16:22 +0900

    Fetch and Store data using API Endpoint

--- api/views.py
+++ api/views.py
@@ -1,12 +1,20 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+from students.models import Student
+from .serializers import StudentSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 
-# Create your views here.
+@api_view(['GET', 'POST'])
 def studentsView(request):
-  students = {
-    'id': 1,
-    'name': 'James',
-    'class': 'Computer Science'
-  }
+  if request.method == 'GET':
+    # Get all the data from the Student table
+    students = Student.objects.all()
+    serializer = StudentSerializer(students, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
   
-  return JsonResponse(students)
+  elif request.method == 'POST':
+    serializer = StudentSerializer(data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
