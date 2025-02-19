b70bbe43fd2f5178568ad632b621ada79f55e9e8
sothulthorn <thornsothul@gmail.com>
2025-02-19 21:10:52 +0900

    Viewset

---
+++ ReadMe.md
@@ -0,0 +1,11 @@
+# Django Rest Framework
+
+## Viewsets
+
+- `viewsets.ViewSet`
+  - list()
+  - create()
+  - retrieve()
+  - update()
+  - delete()
+- `viewsets.ModelViewSet` : Take only queryset and serializer_class an automatically provides both pk based and non-pk based operations.
--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,16 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('employees', views.EmployeeViewset, basename='employee')
 
 urlpatterns = [
     path("students/", views.studentsView),
     path("student/<int:pk>", views.studentDetailView),
     
-    path("employees/", views.Employees.as_view()),
-    path("employee/<int:pk>", views.EmployeeDetail.as_view())
+    # path("employees/", views.Employees.as_view()),
+    # path("employee/<int:pk>", views.EmployeeDetail.as_view()),
+    
+    path('', include(router.urls))
 ]
